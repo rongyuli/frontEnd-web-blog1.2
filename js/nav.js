9948f74c1e3750e639693f01063f97d6858fb7a5
容于力 <dfssd>
2020-11-16 16:47:44 +0800

    基本完成所有页面的移动端适配

--- js/nav.js
+++ js/nav.js
@@ -1,28 +1,25 @@
-$().ready(function(){ 
-    let tokenEl = $('.nav_btn:eq('+ hash +')')
-    tokenEl.css({'color': '#ffba00'})
-    $('#bar').css({'display': 'block','left': tokenEl.offset().left})
-
-    window.onresize = () =>  $('#bar').css({'left': tokenEl.offset().left })
+$().ready(function () {
+    window.onresize = () => $('#bar').css({ left: tokenEl.offset().left })
     $('.nav_btn').mouseover(function () {
-        $('#bar').css({'display': 'block','left': $(this).offset().left})
-        $(this).siblings().css({'color': '#081272'})
-        $(this).css({'color': '#ffba00'})
+        $('#bar').css({ display: 'block', left: $(this).offset().left })
+        $(this).siblings().css({ color: '#081272' })
+        $(this).css({ color: '#ffba00' })
+    })
+    let tokenEl = $('.nav_btn:eq(' + hash + ')')
+    tokenEl.css({ color: '#ffba00' })
+    $('#bar').css({ display: 'block', left: tokenEl.offset().left })
+
+    $('#submitBtn').click(function () {
+        $('#bar').css({ left: tokenEl.offset().left })
     })
 
-    $('#submitBtn').click(function(){
-        $('#bar').css({'left': tokenEl.offset().left })
+    $('nav').mouseout(function () {
+        $('#bar').css({ display: 'block', left: tokenEl.offset().left })
+        tokenEl.siblings().css({ color: '#081272' })
+        tokenEl.css({ color: '#ffba00' })
     })
 
-    $('nav').mouseout(
-        function(){
-            $('#bar').css({'display': 'block','left': tokenEl.offset().left})
-            tokenEl.siblings().css({'color': '#081272'})
-            tokenEl.css({'color': '#ffba00'})
-        }
-    )
-    
-    $('#nav li').mouseout(function(event){
-        event.stopPropagation();
+    $('#nav li').mouseout(function (event) {
+        event.stopPropagation()
     })
 })
